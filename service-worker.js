9dcd26e49e4683d2678bec08ee3362daeeae5339
stefwxters <74666440+stefwxters@users.noreply.github.com>
2025-03-26 11:46:12 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,46 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'webp-converter-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/favicon.png',
+    '/apple-touch-icon.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/browser-image-compression/2.0.2/browser-image-compression.js'
+];
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open("image2webp-cache").then((cache) => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/manifest.json",
-                "/service-worker.js",
-                "/icons/icon-192x192.png",
-                "/icons/icon-512x512.png"
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
     );
-    self.skipWaiting(); // Forces installation immediately
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== "image2webp-cache") {
-                        console.log("Deleting old cache:", cache);
-                        return caches.delete(cache);
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
                     }
                 })
             );
-        })
-    );
-    self.clients.claim(); // Take control of open clients
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
         })
     );
 });
