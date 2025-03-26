cf596f5bcd11fe50c94dfa4f9f716382156e0475
stefwxters <74666440+stefwxters@users.noreply.github.com>
2025-03-26 10:27:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-const CACHE_NAME = "webp-converter-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/service-worker.js",
-  "/styles.css",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+    event.waitUntil(
+        caches.open("image2webp-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/service-worker.js",
+                "/icons/icon-192x192.png",
+                "/icons/icon-512x512.png"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
